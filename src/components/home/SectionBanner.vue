8891af6f7bb2b44ad8b7ddd16240b26ac27485f7
AsyaNext <nastushaburd@mail.ru>
2023-01-07 14:25:13 +0300

    fix: image in banner

--- src/components/home/SectionBanner.vue
+++ src/components/home/SectionBanner.vue
@@ -17,27 +17,12 @@
         </div>
       </div>
     </div>
-<!--    <div class="s-wrap s-type-1" role="slider">-->
-<!--      <input type="radio" id="s-1" name="slider-control" checked="checked">-->
-<!--      <input type="radio" id="s-2" name="slider-control">-->
-<!--      <input type="radio" id="s-3" name="slider-control">-->
-<!--      <ul class="s-content">-->
-<!--        <li class="s-item s-item-1"></li>-->
-<!--        <li class="s-item s-item-2"></li>-->
-<!--        <li class="s-item s-item-3"></li>-->
-<!--      </ul>-->
-<!--      <div class="s-control">-->
-<!--        <label class="s-c-1" for="s-1"></label>-->
-<!--        <label class="s-c-2" for="s-2"></label>-->
-<!--        <label class="s-c-3" for="s-3"></label>-->
-<!--      </div>-->
-<!--    </div>-->
     <div class="banner__slider">
       <carousel ref="myCarousel" :items-to-show="1">
         <slide v-for="slide in 3" :key="slide">
           <div
             class="banner__slide"
-            :style="{ backgroundImage: `url(${require('../../assets/images/banner-image.png')})` }"
+            :style="{ backgroundImage: `url(${require(`../../assets/images/banner-image${slide}.png`)})` }"
           ></div>
         </slide>
       </carousel>
